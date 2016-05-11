337b9d60d7697960aec4f8991b63bedc985e8292
rpsaavedra@uc.cl <rpsaavedra@uc.c>
2016-05-11 18:42:39 -0300

    readme editado con test editado

--- Test.rb
+++ Test.rb
@@ -5,7 +5,7 @@ class TestSimpleNumber < Test::Unit::TestCase
  
   def test_simple
     assert_equal(4, SimpleNumber.new(2).add(2) )
-    assert_equal(6, SimpleNumber.new(4).multiply(3) )
+    assert_equal(6, SimpleNumber.new(2).multiply(3) )
   end
  
 end
